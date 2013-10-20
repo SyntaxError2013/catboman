01cfbffc92e1c4601569d00418664adebea0f79a
jaindeepali <djdeepalijain811@gmail.com>
2013-10-20 12:16:41 +0530

    added feeds page

---
+++ feeds.html.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en" class="yui3-js-enabled wf-omnespro-n2-active wf-omnespro-n3-active wf-proximanova-n1-active wf-proximanova-n4-active wf-proximanova-n7-active wf-active">
+
+    <head>
+        <meta charset="utf-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <meta name="description" content="A layout example that shows off a responsive photo gallery.">
+
+        <title>Pensieve</title>
+
+        <link href='http://fonts.googleapis.com/css?family=Asul' rel='stylesheet' type='text/css'>
+
+        <link rel="stylesheet" href="assets/css/pure-min.css">
+
+        <link rel="stylesheet" href="assets/css/gallery.css">
+
+        <link rel="stylesheet" href="assets/css/main.css">
+
+    </head>
+
+    <body>
+
+        <div>
+            <div class="header">
+                <div class="pure-menu pure-menu-open pure-menu-horizontal">
+                    <ul>
+                        <li class="pure-menu-selected"><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul>
+                    <ul style="float:right;">
+                      <li><a href="" id="disconnect">Sign Out</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="pure-g-r">
+                <div class="pure-u-1-5 translucent-sidebar">
+                  <div class="pure-g-r" style="content-align:center;">
+                    <a class="heading-side" href="">Pensieve</a>
+                  </div>
+                  <div id="authOps">
+                    <div id="profile">xkclmd<br>czbxhjcgzsyu<br>zxhbjvgsduyz<br></div>
+                    <!--<div id="visiblePeople"></div>
+                    <pre id="authResult"></pre>-->
+                  </div>
+                  <div class="pure-button">Download this album</div>
+                </div>
+                <div class="pure-u-4-5 grid">
+                    <div class="pure-u-1-3 photo-box">
+                        <a href="http://www.flickr.com/photos/95572727@N00/4070581709/">
+                            <img src="http://farm3.staticflickr.com/2447/4070581709_1204f25e3b.jpg" alt="Photo of a Bamboo forest in Kyoto.">
+                        </a>
+
+                        <aside class="photo-box-caption">
+                            The Bamboo Forest, Kyoto
+                            <span>
+                                by <a href="http://www.flickr.com/photos/95572727@N00/4070581709/">Stuck in Customs</a> / <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/">cc</a>
+                            </span>
+                        </aside>
+                    </div>
+
+                    <div class="pure-u-1-3 photo-box">
+                        <a href="http://www.flickr.com/photos/ecstaticist/4015614533/">
+                            <img src="http://farm3.staticflickr.com/2785/4015614533_3e04ac2c7d.jpg" alt="Photo of Dahlia taken using an HDR technique.">
+                        </a>
+
+                        <aside class="photo-box-caption">
+                            HDR Dahlia
+                            <span>
+                                by <a href="http://www.flickr.com/photos/ecstaticist/4015614533/">ecstaticist</a> / <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/">cc</a>
+                            </span>
+                        </aside>
+                    </div>
+
+                    <div class="pure-u-1-3 photo-box">
+                        <a href="http://www.flickr.com/photos/blmiers2/6159637428/">
+                            <img src="http://farm7.staticflickr.com/6151/6159637428_6bffb5bce1.jpg" alt="Photo of a misty morning sunrise in Alaska.">
+                        </a>
+
+                        <aside class="photo-box-caption">
+                            Misty Morning Sunrise, Alaska
+                            <span>
+                                by <a href="http://www.flickr.com/photos/blmiers2/6159637428/">blmiers2</a> / <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/">cc</a>
+                            </span>
+                        </aside>
+                    </div>
+
+                    <div class="pure-u-2-3 text-box">
+                        <div class="l-box">
+                            <h1 class="text-box-head">G+ Status</h1>
+                            <p class="text-box-subhead">Date/Plus one's etc.</p>
+                        </div>
+                    </div>
+
+                    <div class="pure-u-1-3 photo-box">
+                        <a href="http://www.flickr.com/photos/blmiers2/6167391543/">
+                            <img src="http://farm7.staticflickr.com/6171/6167391543_395a7cabfd.jpg" alt="Photo of a mountain in Alaska's Denali.">
+                        </a>
+
+                        <aside class="photo-box-caption">
+                            Mountain, Alaska's Denali
+                            <span>
+                                by <a href="http://www.flickr.com/photos/blmiers2/6159637428/">blmiers2</a> / <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/">cc</a>
+                            </span>
+                        </aside>
+                    </div>
+
+                    <div class="pure-u-2-3 photo-box">
+                        <a href="http://www.flickr.com/photos/jjjohn/2120309884/">
+                            <img src="http://farm3.staticflickr.com/2109/2120309884_de48fdb9fe.jpg" alt="Photo of Earth's sky.">
+                        </a>
+
+                        <aside class="photo-box-caption">
+                            Fire, Air, Earth and Water
+                            <span>
+                                by <a href="http://www.flickr.com/photos/jjjohn/2120309884/">jjjohn</a> / <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/">cc</a>
+                            </span>
+                        </aside>
+                    </div>
+
+                    <div class="pure-u-1-3 photo-box">
+                        <a href="http://www.flickr.com/photos/betta_design/2086852016/">
+                            <img src="http://farm3.staticflickr.com/2388/2086852016_5a58dd1881.jpg" alt="Photo of the Taj Mahal at sunset.">
+                        </a>
+
+                        <aside class="photo-box-caption">
+                            Taj Mahal at sunset
+                            <span>
+                                by <a href="http://www.flickr.com/photos/betta_design/2086852016/">betta_design</a> / <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/">cc</a>
+                            </span>
+                        </aside>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer">
+                <div class="g-plusone" style="float:left;" data-href="" data-annotation="inline" data-width="300">
+                </div>
+            </div>
+        </div>  
+    </body>
+        <script type="text/javascript">
+          (function() {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+            po.src = 'https://apis.google.com/js/plusone.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+          })();
+        </script>
+
+        <script src="http://yui.yahooapis.com/3.12.0/build/yui/yui-min.js"></script>
+
+        <div id="yui3-css-stamp" style="position: absolute !important; visibility: hidden !important" class=""></div>
+
+        <script>
+            YUI().use('node-base', 'node-event-delegate', function (Y) {
+                // This just makes sure that the href="#" attached to the <a> elements
+                // don't scroll you back up the page.
+                Y.one('body').delegate('click', function (e) {
+                    e.preventDefault();
+                }, 'a[href="#"]');
+            });
+        </script>
+        </script>
+</html>
